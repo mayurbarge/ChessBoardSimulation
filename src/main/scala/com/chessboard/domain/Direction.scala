596fad4c931a0aa277754e8e0ad257d400d7a3fa
Mayur Barge <mayurbarge226@gmail.com>
2024-09-19 10:40:09 +0530

    Commit #6 | Mayur: Refactored Direction class

--- src/main/scala/com/chessboard/domain/Direction.scala
+++ src/main/scala/com/chessboard/domain/Direction.scala
@@ -1,37 +1,37 @@
 package com.chessboard.domain
 
 trait Direction {
-  val transformBy: Int => Cell => Cell
+  val towardsBy: Int => Cell => Cell
 }
 case object East extends Direction {
-  override val transformBy: Int => Cell => Cell =
+  override val towardsBy: Int => Cell => Cell =
     (steps: Int) => (previousPosition: Cell) => Cell(previousPosition.columnValue + steps, previousPosition.row)
 }
 case object West extends Direction {
-  override val transformBy: Int => Cell => Cell =
+  override val towardsBy: Int => Cell => Cell =
     (steps: Int) => (previousPosition: Cell) => Cell(previousPosition.columnValue - steps, previousPosition.row)
 }
 case object North extends Direction {
-  override val transformBy: Int => Cell => Cell =
+  override val towardsBy: Int => Cell => Cell =
     (steps: Int) => (previousPosition: Cell) => Cell(previousPosition.columnValue, previousPosition.row + steps)
 }
 case object South extends Direction {
-  override val transformBy: Int => Cell => Cell =
+  override val towardsBy: Int => Cell => Cell =
     (steps: Int) => (previousPosition: Cell) => Cell(previousPosition.columnValue, previousPosition.row - steps)
 }
 case object NorthEast extends Direction {
-  override val transformBy: Int => Cell => Cell =
-    (steps: Int) => (previousPosition: Cell) => Cell(previousPosition.columnValue + steps, previousPosition.row + steps)
+  override val towardsBy: Int => Cell => Cell =
+    (steps: Int) => North.towardsBy(steps) andThen East.towardsBy(steps)
 }
 case object SouthEast extends Direction {
-  override val transformBy: Int => Cell => Cell =
-    (steps: Int) => (previousPosition: Cell) => Cell(previousPosition.columnValue + steps, previousPosition.row - steps)
+  override val towardsBy: Int => Cell => Cell =
+    (steps: Int) => South.towardsBy(steps) andThen East.towardsBy(steps)
 }
 case object NorthWest extends Direction {
-  override val transformBy: Int => Cell => Cell =
-    (steps: Int) => (previousPosition: Cell) => Cell(previousPosition.columnValue - steps, previousPosition.row + steps)
+  override val towardsBy: Int => Cell => Cell =
+    (steps: Int) => North.towardsBy(steps) andThen West.towardsBy(steps)
 }
 case object SouthWest extends Direction {
-  override val transformBy: Int => Cell => Cell =
-    (steps: Int) => (previousPosition: Cell) => Cell(previousPosition.columnValue - steps, previousPosition.row - steps)
+  override val towardsBy: Int => Cell => Cell =
+    (steps: Int) => South.towardsBy(steps) andThen West.towardsBy(steps)
 }
--- src/test/scala/com/chessboard/domain/DirectionSpec.scala
+++ src/test/scala/com/chessboard/domain/DirectionSpec.scala
@@ -8,55 +8,55 @@ class DirectionSpec extends AnyFunSpec with Matchers {
   describe("Direction") {
     describe("East") {
       it("should transform cell at A3 by 2 places to reach at C3") {
-        East.transformBy(2)(Cell('A', 3)) shouldBe Cell('C', 3)
+        East.towardsBy(2)(Cell('A', 3)) shouldBe Cell('C', 3)
       }
     }
 
     describe("West") {
       it("should transform cell at B2 by 1 places to reach at A2") {
-        West.transformBy(1)(Cell('B', 2)) shouldBe Cell('A', 2)
+        West.towardsBy(1)(Cell('B', 2)) shouldBe Cell('A', 2)
       }
     }
 
     describe("North") {
       it("should transform cell at B2 by 4 places to reach at B6") {
-        North.transformBy(4)(Cell('B', 2)) shouldBe Cell('B', 6)
+        North.towardsBy(4)(Cell('B', 2)) shouldBe Cell('B', 6)
       }
     }
 
     describe("South") {
       it("should transform cell at D2 by 2 places to reach at D1") {
-        South.transformBy(2)(Cell('D', 3)) shouldBe Cell('D', 1)
+        South.towardsBy(2)(Cell('D', 3)) shouldBe Cell('D', 1)
       }
     }
 
     describe("NorthEast") {
       it("should transform cell at D3 by 2 places to reach at F5") {
-        NorthEast.transformBy(2)(Cell('D', 3)).cellNumber shouldBe Cell('F', 5).cellNumber
+        NorthEast.towardsBy(2)(Cell('D', 3)).cellNumber shouldBe Cell('F', 5).cellNumber
       }
     }
 
     describe("NorthWest") {
       it("should transform cell at C3 by 2 places to reach at A5") {
-        NorthWest.transformBy(2)(Cell('C', 3)) shouldBe Cell('A', 5)
+        NorthWest.towardsBy(2)(Cell('C', 3)) shouldBe Cell('A', 5)
       }
     }
 
     describe("SouthEast") {
       it("should transform cell at A6 by 5 places to reach at F1") {
-        SouthEast.transformBy(5)(Cell('A', 6)) shouldBe Cell('F', 1)
+        SouthEast.towardsBy(5)(Cell('A', 6)) shouldBe Cell('F', 1)
       }
     }
 
     describe("SouthWest") {
       it("should transform cell at A1 by 4 places to reach at A1") {
-        SouthWest.transformBy(4)(Cell('E', 5)) shouldBe Cell('A', 1)
+        SouthWest.towardsBy(4)(Cell('E', 5)) shouldBe Cell('A', 1)
       }
     }
 
     describe("West") {
       it("should return invalid cell at A1 when transformed by 2 places") {
-        val cell: Cell = SouthWest.transformBy(2)(Cell('A', 1))
+        val cell: Cell = SouthWest.towardsBy(2)(Cell('A', 1))
         ValidatedCell(cell).leftMap(_.getMessage) shouldBe Validated.Invalid("Cell is invalid.")
       }
     }
