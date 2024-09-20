9fa6c018fdb9602c8a76f9d94cfaceb277cfe11e
Mayur Barge <mayurbarge226@gmail.com>
2024-09-20 13:43:01 +0530

    Commit #12 | Mayur : Refactored moves to extract regular move logic into separate trait

--- src/main/scala/com/chessboard/domain/movements/Move.scala
+++ src/main/scala/com/chessboard/domain/movements/Move.scala
@@ -1,8 +1,7 @@
 package com.chessboard.domain.movements
 
 import com.chessboard.domain._
-import com.chessboard.domain.movements.HorizontalMove.findNextPositions
-import com.chessboard.domain.movements.VerticalMove.findNextPositions
+import com.chessboard.domain.movements.HorizontalMove.{directionsAllowedForMove, findNextPositions}
 sealed trait Move {
   def shift(nthStep: Int, initial: Cell): List[Cell]
   def applyMoves(nthStep: Int, moves: List[(Direction, Int)]): List[Cell => Cell] = {
@@ -24,24 +23,24 @@ sealed trait Move {
     nextPositionsList.map(nextCellOf => nextCellOf(initial))
   }
 }
-case object HorizontalMove extends Move {
+
+trait MoveWithRegularDirections extends Move {
+  val directionsAllowedForMove: List[Direction]
+
   def shift(nthStep: Int, initial: Cell) = {
-    val nextPositionsList = super.applyMoves(MoveDirections.horizontal.map((_, nthStep)))
+    val nextPositionsList = applyMoves(directionsAllowedForMove.map((_, nthStep)))
     findNextPositions(initial, nextPositionsList)
   }
 }
-case object VerticalMove extends Move {
-  def shift(nthStep: Int, initial: Cell) = {
-    val nextPositionsList = super.applyMoves(MoveDirections.vertical.map((_, nthStep)))
-    findNextPositions(initial, nextPositionsList)
-  }
+case object HorizontalMove extends Move with MoveWithRegularDirections {
+  val directionsAllowedForMove = MoveDirections.horizontal
+}
+case object VerticalMove extends Move with MoveWithRegularDirections {
+  val directionsAllowedForMove = MoveDirections.vertical
 }
 
-case object DiagonalMove extends Move {
-  def shift(nthStep: Int, initial: Cell) = {
-    val nextPositionsList = super.applyMoves(MoveDirections.diagonal.map((_, nthStep)))
-    findNextPositions(initial, nextPositionsList)
-  }
+case object DiagonalMove extends Move with MoveWithRegularDirections {
+  val directionsAllowedForMove = MoveDirections.diagonal
 }
 case class ComplexMove(complexMoves: List[List[(Direction, Int)]]) extends Move {
   override def shift(nthStep: Int, initial: Cell): List[Cell] = {
--- src/main/scala/com/chessboard/domain/pieces/Piece.scala
+++ src/main/scala/com/chessboard/domain/pieces/Piece.scala
@@ -13,17 +13,18 @@ trait Piece { self =>
   def possibleMovesAtPosition(steps: Int, currentPosition: Cell) = {
     self.allowedMoves.flatMap(movement => movement.shift(steps, currentPosition))
   }
-  def allSingleStepPossibleMoves(currentPosition: Cell): List[Cell] = {
-    def filterCondition(cellToCheck: Cell) =
-      ValidatedCell(cellToCheck).isValid && currentPosition != cellToCheck
 
-    possibleMovesAtPositionWithFilter(1, currentPosition, filterCondition)
-  }
   def allPossibleMoves(currentPosition: Cell): List[Cell]
 }
 trait SingleStepPiece extends Piece {
   override def allPossibleMoves(currentPosition: Cell): List[Cell] = {
     allSingleStepPossibleMoves(currentPosition)
+  }
+  def allSingleStepPossibleMoves(currentPosition: Cell): List[Cell] = {
+    def filterCondition(cellToCheck: Cell) =
+      ValidatedCell(cellToCheck).isValid && currentPosition != cellToCheck
+
+    possibleMovesAtPositionWithFilter(1, currentPosition, filterCondition)
   }
 }
 case class King(allowedMoves: List[Move]) extends Piece with SingleStepPiece {
